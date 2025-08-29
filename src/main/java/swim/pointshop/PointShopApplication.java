93503a139282dc692547b11ab35ca2cd3667039c
SuYoung Song <prodraft@naver.com>
2025-08-29 13:56:48 +0900

    build : init

---
+++ src/main/java/swim/pointshop/PointShopApplication.java
@@ -0,0 +1,13 @@
+package swim.pointshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/swim/pointshop/PointShopApplicationTests.java
@@ -0,0 +1,13 @@
+package swim.pointshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
